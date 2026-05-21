31ea2680d4eaf7301ff3e149da2910c4f81902e9
makario87 <ariasortiz.luis7@gmail.com>
2026-05-21 20:55:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return "CAZADOR API ONLINE"
 
+@app.route("/webhook")
+def webhook():
+    return "WEBHOOK RECIBIDO"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
     
